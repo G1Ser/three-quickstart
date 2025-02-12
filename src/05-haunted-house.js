acb6652397dab74a24eb344368794e78b2494761
chauncey <chauncey1129@163.com>
2025-02-12 19:55:13 +0800

    05-add grass

--- src/05-haunted-house.js
+++ src/05-haunted-house.js
@@ -1,2 +1,56 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// scene
+const scene = new THREE.Scene();
+
+// light
+const ambientLight = new THREE.AmbientLight(0x404040);
+// add light to scene
+scene.add(ambientLight);
+
+// camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+// set camera position
+camera.position.set(0, 0, 1);
+// add camera to scene
+scene.add(camera);
+
+// renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// animation
+const tick = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+// grass
+const planeGemotry = new THREE.PlaneGeometry(1, 1);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const plane = new THREE.Mesh(planeGemotry,planeMaterial);
+// add grass to scene
+scene.add(plane);
+
+tick();
