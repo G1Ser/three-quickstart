41d62c1fc6e445a26feeec3be55b6ed4cf4a5560
Chauncey <chauncey1129@163.com>
2025-02-13 01:15:01 +0800

    05-grass done

--- src/05-haunted-house.js
+++ src/05-haunted-house.js
@@ -5,7 +5,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 const scene = new THREE.Scene();
 
 // light
-const ambientLight = new THREE.AmbientLight(0x404040);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 // add light to scene
 scene.add(ambientLight);
 
@@ -17,7 +17,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 // set camera position
-camera.position.set(0, 0, 1);
+camera.position.set(4, 2, 5);
 // add camera to scene
 scene.add(camera);
 
@@ -46,11 +46,35 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 });
 
-// grass
-const planeGemotry = new THREE.PlaneGeometry(1, 1);
-const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-const plane = new THREE.Mesh(planeGemotry,planeMaterial);
-// add grass to scene
-scene.add(plane);
+// texture loader
+const textureLoader = new THREE.TextureLoader();
+const grassBasicColorTexture = textureLoader.load(
+  "/src/assets/grass/BaseColor.jpg"
+);
+const grassAmbientOcclusionTexture = textureLoader.load(
+  "/src/assets/grass/AmbientOcclusion.jpg"
+);
+const grassHeightTexture = textureLoader.load("/src/assets/grass/Height.png");
+const grassNormalTexture = textureLoader.load("/src/assets/grass/Normal.jpg");
+const grassRoughnessTexture = textureLoader.load(
+  "/src/assets/grass/Roughness.jpg"
+);
 
+// grass
+const planeGemotry = new THREE.PlaneGeometry(20, 20, 50, 50);
+const planeMaterial = new THREE.MeshStandardMaterial({
+  map: grassBasicColorTexture,
+  aoMap: grassAmbientOcclusionTexture,
+  displacementMap: grassHeightTexture,
+  normalMap: grassNormalTexture,
+  roughnessMap: grassRoughnessTexture,
+});
+const grass = new THREE.Mesh(planeGemotry, planeMaterial);
+grass.geometry.setAttribute(
+  "uv2",
+  new THREE.Float32BufferAttribute(grass.geometry.attributes.uv.array, 2)
+);
+grass.rotation.x = -Math.PI / 2;
+// add grass to scene
+scene.add(grass);
 tick();
