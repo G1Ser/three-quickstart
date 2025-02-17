478147e10010dc9d6591fdcca234da4d66ab5320
chauncey <chauncey1129@163.com>
2025-02-17 17:40:39 +0800

    05-huanted house 开始制作房子

--- src/05-haunted-house.js
+++ src/05-haunted-house.js
@@ -60,12 +60,31 @@ const grassRoughnessTexture = textureLoader.load(
   "/src/assets/grass/Roughness.jpg"
 );
 
+grassBasicColorTexture.repeat.set(2, 2);
+grassAmbientOcclusionTexture.repeat.set(2, 2);
+grassHeightTexture.repeat.set(2, 2);
+grassNormalTexture.repeat.set(2, 2);
+grassRoughnessTexture.repeat.set(2, 2);
+
+grassBasicColorTexture.wrapS = THREE.RepeatWrapping;
+grassAmbientOcclusionTexture.wrapS = THREE.RepeatWrapping;
+grassHeightTexture.wrapS = THREE.RepeatWrapping;
+grassNormalTexture.wrapS = THREE.RepeatWrapping;
+grassRoughnessTexture.wrapS = THREE.RepeatWrapping;
+
+grassBasicColorTexture.wrapT = THREE.RepeatWrapping;
+grassAmbientOcclusionTexture.wrapT = THREE.RepeatWrapping;
+grassHeightTexture.wrapT = THREE.RepeatWrapping;
+grassNormalTexture.wrapT = THREE.RepeatWrapping;
+grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
+
 // grass
-const planeGemotry = new THREE.PlaneGeometry(20, 20, 50, 50);
+const planeGemotry = new THREE.PlaneGeometry(20, 20, 20, 20);
 const planeMaterial = new THREE.MeshStandardMaterial({
   map: grassBasicColorTexture,
   aoMap: grassAmbientOcclusionTexture,
   displacementMap: grassHeightTexture,
+  displacementScale: 0.35,
   normalMap: grassNormalTexture,
   roughnessMap: grassRoughnessTexture,
 });
@@ -75,6 +94,28 @@ grass.geometry.setAttribute(
   new THREE.Float32BufferAttribute(grass.geometry.attributes.uv.array, 2)
 );
 grass.rotation.x = -Math.PI / 2;
-// add grass to scene
-scene.add(grass);
+
+// house
+const house = new THREE.Group();
+
+// wall
+const wallGeometry = new THREE.BoxGeometry(4, 2.5, 4);
+const wallMaterial = new THREE.MeshStandardMaterial({ color: 0xac8e82 });
+const wall = new THREE.Mesh(wallGeometry, wallMaterial);
+
+wall.position.y = 0.5 * 2.5;
+
+// roof
+const roofGeometry = new THREE.ConeGeometry(3.5, 1, 4);
+const roofMaterial = new THREE.MeshStandardMaterial({ color: 0xb35f45 });
+const roof = new THREE.Mesh(roofGeometry, roofMaterial);
+
+roof.position.y = 2.5 + 0.5 * 1;
+roof.rotation.y = Math.PI * 0.25;
+
+// add component to house
+house.add(wall, roof);
+
+// add object to scene
+scene.add(grass, house);
 tick();
